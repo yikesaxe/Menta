ba041ba7d89bcce7576a6800e65402aa154ffc4c
Axel Livias <yikesaxe@Axels-MBP-5.lan>
2024-07-18 02:15:38 -0400

    Initial commit, setting up Menta's frontend enviornment. Added initial README.

---
+++ menta-frontend/menta-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+export default function App() {
+  return (
+    <h1 className="text-red-400 font-bold underline">
+      Menta!
+    </h1>
+  )
+}
